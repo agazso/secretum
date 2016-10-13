6bddb87c13675d1e25e2ecf405f7d64c56d71c98
Attila Gazso <agazso@gmail.com>
2016-10-14 00:54:00 +0200

    Added reconnect logic to the client

--- src/SockJSClientTransport.ts
+++ src/SockJSClientTransport.ts
@@ -11,18 +11,32 @@ import SockJSClass = __SockJSClient.SockJSClass;
 import http = require('http');
 import stream = require("stream");
 
+type ConnectionState = "connected" | "connecting" | "disconnected";
 
 export class SockJSClientTransport implements ClientTransport {
-    sock: SockJSClass;
+    private sock: SockJSClass;
+    private connectionState: ConnectionState = "disconnected";
+    private messageQueue: SockJSMessage[] = [];
     onReceive: (clientId: string, message: ClientMessage) => void;
 
     constructor(private readonly url: string,
                 private readonly roomName: string, 
                 onConnect: () => void,
                 ) {
-        
+        this.connect(onConnect);
+    }
+
+    private connect(onConnect: () => void) {
+        this.connectionState = "connecting";
+
         this.sock = new SockJS(this.url);
         this.sock.onopen = (e: __SockJSClient.OpenEvent) => {
+            this.connectionState = "connected";
+            
+            while (this.messageQueue.length > 0) {
+                this.sock.send(JSON.stringify(this.messageQueue.shift()));
+            }
+            
             onConnect();
         };
         
@@ -35,6 +49,11 @@ export class SockJSClientTransport implements ClientTransport {
                 console.log(e);
             }
         };
+
+        this.sock.onclose = (e) => {
+            this.connectionState = "disconnected";
+            setTimeout(() => this.connect(onConnect), 1000);
+        }
     }
 
     sendServer(clientId: string, message: ClientMessage): void {
@@ -47,6 +66,11 @@ export class SockJSClientTransport implements ClientTransport {
 
     private sendSockJSMessage(message: SockJSMessage) {
         Debug.log(message);
-        this.sock.send(JSON.stringify(message));
+        
+        if (this.connectionState == "connected") {
+            this.sock.send(JSON.stringify(message));
+        } else {
+            this.messageQueue.push(message);
+        }
     }
 }
