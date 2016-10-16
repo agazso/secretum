6149ce5d92b32df48fb7d2f73daefd4a0a91d81d
Attila Gazso <agazso@gmail.com>
2016-10-17 00:18:14 +0200

    Fixed build script

--- tscbuild.cmd
+++ tscbuild.cmd
@@ -4,6 +4,8 @@ for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
 echo ------------ [%ldt%]
 
-tsc %*
-echo "browserify"
-browserify -o bin/app.js build/src/client/BrowserChatApplication.js
+echo Compiling...
+call tsc %* || exit /b %ERRORLEVEL%
+echo Calling browserify...
+call browserify -o bin/app.js build/src/client/BrowserChatApplication.js || exit /b %ERRORLEVEL%
+
